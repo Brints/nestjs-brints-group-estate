aaff8c5b6fa90494b0ce585dd7bb4857a355f9c2
aniebietafia <aniebietafia87@gmail.com>
2024-08-24 14:59:27 +0100

    refactor: updated packages

--- brints-estate-api/src/main.ts
+++ brints-estate-api/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  logger.log('Application started on http://localhost:3000');
 }
 bootstrap();
