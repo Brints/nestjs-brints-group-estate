ea21c018365ab23d35e4b05fad32c026dd222158
aniebietafia <aniebietafia87@gmail.com>
2024-08-24 15:32:42 +0100

    changed local running port to 3001

--- brints-estate-api/src/main.ts
+++ brints-estate-api/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  logger.log('Application started on http://localhost:3000');
+  await app.listen(3001);
+  logger.log('Application started on http://localhost:3001');
 }
 bootstrap();
