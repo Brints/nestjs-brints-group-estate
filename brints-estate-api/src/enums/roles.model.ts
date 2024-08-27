c052f605eba87d06fae7f111ffd3d7fb9c185633
aniebietafia <aniebietafia87@gmail.com>
2024-08-27 22:10:37 +0100

    feat: adding enums

---
+++ brints-estate-api/src/enums/roles.model.ts
@@ -0,0 +1,31 @@
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+  LANDLORD = 'landlord',
+  REALTOR = 'realtor',
+}
+
+export enum UserGender {
+  MALE = 'male',
+  FEMALE = 'female',
+}
+
+export enum UserTitle {
+  MR = 'mr',
+  MRS = 'mrs',
+  MISS = 'miss',
+  DR = 'dr',
+}
+
+export enum AccountStatus {
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+  SUSPENDED = 'suspended',
+  BLOCKED = 'blocked',
+}
+
+export enum VerificationStatus {
+  VERIFIED = 'verified',
+  PENDING = 'pending',
+  EXPIRED = 'expired',
+}
