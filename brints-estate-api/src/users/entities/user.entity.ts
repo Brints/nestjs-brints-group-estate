4fad121bc6f862338b7a08de3fa6892b19386fea
aniebietafia <aniebietafia87@gmail.com>
2024-08-27 16:23:29 +0100

    feat: connecting the database

--- brints-estate-api/src/users/entities/user.entity.ts
+++ brints-estate-api/src/users/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @Column()
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
