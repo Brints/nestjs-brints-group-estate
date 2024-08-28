2239dc99a9f03aba5b869115bb8b123824b2a3f7
aniebietafia <aniebietafia87@gmail.com>
2024-08-28 12:20:22 +0100

    feat: app module

--- brints-estate-api/src/app.module.ts
+++ brints-estate-api/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -11,6 +12,18 @@ import { UsersModule } from './users/users.module';
     }),
     AuthModule,
     UsersModule,
+    // TypeOrmModule.forRootAsync({
+    //   useFactory: () => ({
+    //     type: 'postgres',
+    //     host: 'localhost',
+    //     port: 5432,
+    //     username: 'postgres',
+    //     password: 'postgres',
+    //     database: 'brints-estate-backend',
+    //     synchronize: true,
+    //     autoLoadEntities: true,
+    //   })
+    // })
   ],
 })
 export class AppModule {}
