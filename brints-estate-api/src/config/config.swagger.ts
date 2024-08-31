ec92dd0dac1a9afa8aebd543647c7c7e69b7fac8
aniebietafia <aniebietafia87@gmail.com>
2024-08-31 16:50:32 +0100

    feat: swagger documentation

---
+++ brints-estate-api/src/config/config.swagger.ts
@@ -0,0 +1,21 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function swaggerInitializer(app: INestApplication<any>) {
+  const config = new DocumentBuilder()
+    .setTitle('Brints Estate API')
+    .setDescription(
+      'The modern day Real Estate API simplifying the way we buy and sell properties',
+    )
+    .addServer('http://localhost:3001', 'Development Server')
+    .addServer('https://brints-estate-api.herokuapp.com', 'Production Server')
+    .setTermsOfService('http://localhost:3001/terms')
+    .setLicense(
+      'MIT',
+      'https://github.com/Brints/nestjs-brints-group-estate/blob/main/LICENSE',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+}
