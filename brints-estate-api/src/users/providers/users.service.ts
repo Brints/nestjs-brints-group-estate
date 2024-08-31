e057da66ce2528e7e278d90c7333aeaa4ffc8bd4
aniebietafia <aniebietafia87@gmail.com>
2024-08-31 18:59:39 +0100

    feat: user service

--- brints-estate-api/src/users/providers/users.service.ts
+++ brints-estate-api/src/users/providers/users.service.ts
@@ -1,12 +1,6 @@
 import { Injectable } from '@nestjs/common';
-// import { CreateUserDto } from '../dto/create-user.dto';
-// import { CreateAuthDto } from 'src/auth/dto/create-auth.dto';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { Repository } from 'typeorm';
-// import { User } from '../entities/user.entity';
-// import { UserAuth } from '../entities/userAuth.entity';
 
 @Injectable()
 export class UsersService {
-  constructor() {} // private readonly userAuthRepository: Repository<UserAuth>, // private readonly userRepository: Repository<User>, // @InjectRepository(User)
+  constructor() {}
 }
