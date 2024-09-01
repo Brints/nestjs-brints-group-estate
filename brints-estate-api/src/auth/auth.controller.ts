d3ab9584c88831d8fa84266563f0366572798e54
aniebietafia <aniebietafia87@gmail.com>
2024-09-01 17:37:40 +0100

    feat: auth controller

--- brints-estate-api/src/auth/auth.controller.ts
+++ brints-estate-api/src/auth/auth.controller.ts
@@ -1,16 +1,14 @@
-import { Controller, Post, Body, HttpStatus, UseFilters } from '@nestjs/common';
+import { Controller, Post, Body, HttpStatus } from '@nestjs/common';
 import { AuthService } from './providers/auth.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { CreateUserAuthDto } from 'src/users/dto/create-userauth.dto';
 import { ApiTags } from '@nestjs/swagger';
-import { HttpExceptionFilter } from 'src/exceptions/http-exception.filter';
 
 @Controller('auth')
 @ApiTags('Auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseFilters(HttpExceptionFilter)
   @Post('register')
   async registerUser(
     @Body()
