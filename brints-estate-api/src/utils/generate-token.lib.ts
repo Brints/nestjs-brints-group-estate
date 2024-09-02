e0875595c4bd6dfa735dc30a0e445323655eb13b
aniebietafia <aniebietafia87@gmail.com>
2024-09-02 17:43:08 +0100

    feat: building generation token class

--- brints-estate-api/src/utils/generate-token.lib.ts
+++ brints-estate-api/src/utils/generate-token.lib.ts
@@ -1,11 +1,25 @@
 import * as crypto from 'crypto';
 
-export function generateVerificationToken() {
-  return crypto.randomBytes(40).toString('hex');
-}
+// export function generateVerificationToken() {
+//   return crypto.randomBytes(40).toString('hex');
+// }
 
-export function generateOTP(length: number) {
-  return Math.floor(100000 + Math.random() * 900000)
-    .toString()
-    .slice(0, length);
+// export function generateOTP(length: number) {
+//   return Math.floor(100000 + Math.random() * 900000)
+//     .toString()
+//     .slice(0, length);
+// }
+
+export class GenerateTokenHelper {
+  constructor() {}
+
+  public generateVerificationToken() {
+    return crypto.randomBytes(40).toString('hex');
+  }
+
+  public generateOTP(length: number) {
+    return Math.floor(100000 + Math.random() * 900000)
+      .toString()
+      .slice(0, length);
+  }
 }
