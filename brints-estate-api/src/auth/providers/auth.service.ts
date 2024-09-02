5e1ec3d51205c4b78e139e280062a11892d43bb1
aniebietafia <aniebietafia87@gmail.com>
2024-09-02 14:49:25 +0100

    feat: implemented login user service

--- brints-estate-api/src/auth/providers/auth.service.ts
+++ brints-estate-api/src/auth/providers/auth.service.ts
@@ -3,15 +3,24 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { CreateUserAuthDto } from 'src/users/dto/create-userauth.dto';
 import { CreateUserProvider } from './create-user.provider';
+import { LoginUserDto } from '../dto/login.dto';
+import { LoginUserProvider } from './login-user.provider';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly createUserProvider: CreateUserProvider) {}
+  constructor(
+    private readonly createUserProvider: CreateUserProvider,
+    private readonly loginUserProvider: LoginUserProvider,
+  ) {}
 
   public async createUser(
     createUserDto: CreateUserDto,
     createUserAuthDto: CreateUserAuthDto,
   ) {
     return this.createUserProvider.createUser(createUserDto, createUserAuthDto);
+  }
+
+  public async loginUser(loginUserDto: LoginUserDto) {
+    return this.loginUserProvider.loginUser(loginUserDto);
   }
 }
