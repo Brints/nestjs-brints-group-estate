8d670ccce636694d33bb88983a034b57a15432a9
aniebietafia <aniebietafia87@gmail.com>
2024-09-02 14:46:29 +0100

    feat: implemented create user service

--- brints-estate-api/src/auth/providers/auth.service.ts
+++ brints-estate-api/src/auth/providers/auth.service.ts
@@ -1,79 +1,17 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
 
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { CreateUserAuthDto } from 'src/users/dto/create-userauth.dto';
-import { User } from 'src/users/entities/user.entity';
-import { UserAuth } from 'src/users/entities/userAuth.entity';
-import {
-  generateOTP,
-  generateVerificationToken,
-} from 'src/utils/generate-token.lib';
-import { VerificationStatus } from 'src/enums/roles.model';
-import { CustomConflictException } from 'src/exceptions/custom.exception';
+import { CreateUserProvider } from './create-user.provider';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
-    @InjectRepository(UserAuth)
-    private readonly userAuthRepository: Repository<UserAuth>,
-  ) {}
+  constructor(private readonly createUserProvider: CreateUserProvider) {}
 
   public async createUser(
     createUserDto: CreateUserDto,
     createUserAuthDto: CreateUserAuthDto,
   ) {
-    const userExists = await this.userRepository.findOne({
-      where: { email: createUserDto.email },
-    });
-
-    if (userExists) {
-      throw new CustomConflictException(
-        HttpStatus.CONFLICT,
-        'User Exists already',
-      );
-    }
-
-    const user = this.userRepository.create(createUserDto);
-
-    const verificationToken = generateVerificationToken();
-    const verificationTokenExpiry = new Date();
-    verificationTokenExpiry.setHours(verificationTokenExpiry.getHours() + 1);
-
-    const newOtp = generateOTP(6);
-    const otpExpiry = new Date();
-    otpExpiry.setMinutes(otpExpiry.getMinutes() + 20);
-
-    const emailVerificationToken = verificationToken;
-    const emailVerificationTokenExpiresIn = verificationTokenExpiry;
-
-    const otp = parseInt(newOtp);
-    const otpExpiresIn = otpExpiry;
-
-    const isEmailVerified = false;
-    const isPhoneNumberVerified = false;
-    const status = VerificationStatus.PENDING;
-
-    const userAuth = this.userAuthRepository.create({
-      ...createUserAuthDto,
-      emailVerificationToken,
-      emailVerificationTokenExpiresIn,
-      otp,
-      otpExpiresIn,
-      isEmailVerified,
-      isPhoneNumberVerified,
-      status,
-      user: user,
-    });
-
-    user.user_auth = userAuth;
-
-    await this.userAuthRepository.save(userAuth);
-    await this.userRepository.save(user);
-
-    return user;
+    return this.createUserProvider.createUser(createUserDto, createUserAuthDto);
   }
 }
