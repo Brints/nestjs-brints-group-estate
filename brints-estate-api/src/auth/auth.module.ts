573863b1062e492ce4092669fc59f56b2f9a4c16
aniebietafia <aniebietafia87@gmail.com>
2024-09-03 13:15:38 +0100

    feat: injected JwtModule

--- brints-estate-api/src/auth/auth.module.ts
+++ brints-estate-api/src/auth/auth.module.ts
@@ -1,17 +1,22 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './providers/auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { HashingProvider } from './providers/hashing.provider';
 import { BcryptProvider } from './providers/bcrypt.provider';
 import { CreateUserProvider } from './providers/create-user.provider';
-import { UserHelper } from 'src/utils/userHelper.lib';
-import { GenerateTokenHelper } from 'src/utils/generate-token.lib';
+import { UserHelper } from '../utils/userHelper.lib';
+import { GenerateTokenHelper } from '../utils/generate-token.lib';
 import { LoginUserProvider } from './providers/login-user.provider';
+import jwtConfig from './config/jwt.config';
 
 @Module({
   controllers: [AuthController],
   providers: [
+    ConfigService,
     AuthService,
     CreateUserProvider,
     LoginUserProvider,
@@ -22,7 +27,11 @@ import { LoginUserProvider } from './providers/login-user.provider';
       useClass: BcryptProvider,
     },
   ],
-  imports: [forwardRef(() => UsersModule)],
+  imports: [
+    forwardRef(() => UsersModule),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
   exports: [AuthService, HashingProvider],
 })
 export class AuthModule {}
