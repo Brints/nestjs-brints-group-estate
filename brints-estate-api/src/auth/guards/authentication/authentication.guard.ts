a6038cf77394f6fdb98b59108fedcb2dec95109d
aniebietafia <aniebietafia87@gmail.com>
2024-09-04 10:18:40 +0100

    feat: authentication guard

---
+++ brints-estate-api/src/auth/guards/authentication/authentication.guard.ts
@@ -0,0 +1,59 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AccessTokenGuard } from '../access-token/access-token.guard';
+import { AuthType } from 'src/auth/enum/auth-type.enum';
+import { AUTH_TYPE_KEY } from 'src/auth/constants/auth.constants';
+import { CustomException } from 'src/exceptions/custom.exception';
+
+@Injectable()
+export class AuthenticationGuard implements CanActivate {
+  private static readonly defaultAuthType = AuthType.Bearer;
+
+  private get authTypeGuardMap(): Record<
+    AuthType,
+    CanActivate | CanActivate[]
+  > {
+    return {
+      [AuthType.Bearer]: this.accessTokenGuard,
+      [AuthType.None]: { canActivate: () => true },
+    };
+  }
+
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly accessTokenGuard: AccessTokenGuard,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const authTypes = this.reflector.getAllAndOverride<AuthType[]>(
+      AUTH_TYPE_KEY,
+      [context.getHandler(), context.getClass()],
+    ) ?? [AuthenticationGuard.defaultAuthType];
+
+    const guards = authTypes
+      .map((authType) => this.authTypeGuardMap[authType])
+      .flat();
+
+    const error = new CustomException(HttpStatus.UNAUTHORIZED, 'Unauthorized');
+
+    for (const instance of guards) {
+      const canActivate = await Promise.resolve(
+        instance.canActivate(context),
+      ).catch((err) => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        error: err;
+      });
+
+      if (!canActivate) {
+        throw error;
+      }
+    }
+
+    return true;
+  }
+}
