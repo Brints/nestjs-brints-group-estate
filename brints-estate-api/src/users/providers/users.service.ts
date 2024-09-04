b08ddd98d57740d82f16c0707d73fdfb3ebad040
aniebietafia <aniebietafia87@gmail.com>
2024-09-04 13:25:23 +0100

    initializing users service

--- brints-estate-api/src/users/providers/users.service.ts
+++ brints-estate-api/src/users/providers/users.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IActiveUser } from 'src/auth/interfaces/active-user.interface';
+import { User } from '../entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  constructor() {}
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
 
-  public async getAllUsers() {
-    return 'Hello World';
+  public async getAllUsers(user: IActiveUser) {
+    return user;
+  }
+
+  public async getAll() {
+    const users = await this.userRepository.find();
+    return users;
   }
 }
