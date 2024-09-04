33f31f05e74157e8537259adcb1ab6cdcff415e1
aniebietafia <aniebietafia87@gmail.com>
2024-09-04 13:25:54 +0100

    initializing users controller

--- brints-estate-api/src/users/users.controller.ts
+++ brints-estate-api/src/users/users.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UsersService } from './providers/users.service';
+import { ActiveUser } from 'src/auth/decorators/active-user.decorator';
+import { IActiveUser } from 'src/auth/interfaces/active-user.interface';
 
 @Controller('users')
 @ApiTags('Users')
@@ -8,7 +10,22 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('all')
-  async getUser() {
-    return this.usersService.getAllUsers();
+  async getUser(@ActiveUser() user: IActiveUser) {
+    const data = await this.usersService.getAllUsers(user);
+    return {
+      message: 'Retrieving successful',
+      status_code: HttpStatus.OK,
+      data,
+    };
+  }
+
+  @Get()
+  async getUsers() {
+    const result = await this.usersService.getAll();
+    return {
+      message: 'All Users',
+      status_code: HttpStatus.OK,
+      result,
+    };
   }
 }
