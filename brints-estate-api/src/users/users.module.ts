d84b1e715e23ea198502e7ef97380e58a4d932a1
aniebietafia <aniebietafia87@gmail.com>
2024-09-04 10:27:16 +0100

    feat: users module

--- brints-estate-api/src/users/users.module.ts
+++ brints-estate-api/src/users/users.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UsersController } from './users.controller';
 import { UsersService } from './providers/users.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 import { User } from './entities/user.entity';
 import { UserAuth } from './entities/userAuth.entity';
@@ -10,7 +11,10 @@ import { UserAuth } from './entities/userAuth.entity';
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [TypeOrmModule.forFeature([User, UserAuth])],
+  imports: [
+    TypeOrmModule.forFeature([User, UserAuth]),
+    forwardRef(() => AuthModule),
+  ],
   exports: [TypeOrmModule, UsersService],
 })
 export class UsersModule {}
