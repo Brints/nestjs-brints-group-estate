862da2f4463e6de1b8c3208ceca56dedbd55fd50
aniebietafia <aniebietafia87@gmail.com>
2024-09-06 20:11:05 +0100

    feat: upload entity

---
+++ brints-estate-api/src/uploads/upload.entity.ts
@@ -0,0 +1,27 @@
+import { AbstractBaseEntity } from 'src/base.entity';
+import { Column, Entity } from 'typeorm';
+import { FileTypes } from './enums/file-types.enum';
+
+@Entity()
+export class Upload extends AbstractBaseEntity {
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  file_name: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  file_path: string;
+
+  @Column({
+    type: 'enum',
+    enum: FileTypes,
+    length: 255,
+    nullable: false,
+    default: FileTypes.IMAGE,
+  })
+  file_type: string;
+
+  @Column({ type: 'varchar', length: 128, nullable: false })
+  file_mime: string;
+
+  @Column({ type: 'numeric', length: 50, nullable: false })
+  file_size: number;
+}
