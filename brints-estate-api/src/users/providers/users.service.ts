5ee1b281c744f32b6e026c65d41e4d1e9b0a9409
aniebietafia <aniebietafia87@gmail.com>
2024-09-09 08:16:25 +0100

    feat: implemented verify user email and phone number service

--- brints-estate-api/src/users/providers/users.service.ts
+++ brints-estate-api/src/users/providers/users.service.ts
@@ -3,13 +3,31 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { IActiveUser } from 'src/auth/interfaces/active-user.interface';
 import { User } from '../entities/user.entity';
 import { Repository } from 'typeorm';
+import { VerifyEmailDto } from '../dto/verify-email.dto';
+import { VerifyPhoneNumberDto } from '../dto/verify-phone-number.dto';
+import { VerifyEmailProvider } from './verify-email.provider';
+import { VerifyPhoneNumberProvider } from './verify-phone-number.provider';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
-    private userRepository: Repository<User>,
+    private readonly userRepository: Repository<User>,
+
+    private readonly verifyEmailProvider: VerifyEmailProvider,
+
+    private readonly verifyPhoneNumberProvider: VerifyPhoneNumberProvider,
   ) {}
+
+  public async verifyUserEmail(verifyEmailDto: VerifyEmailDto) {
+    return this.verifyEmailProvider.verifyUserEmail(verifyEmailDto);
+  }
+
+  public async verifyPhoneNumber(verifyPhoneNumberDto: VerifyPhoneNumberDto) {
+    return this.verifyPhoneNumberProvider.verifyPhoneNumber(
+      verifyPhoneNumberDto,
+    );
+  }
 
   public async getAllUsers(user: IActiveUser) {
     return user;
