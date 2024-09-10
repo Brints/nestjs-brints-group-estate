60aa027973ebe993955c8af7887bc9b79dcefd9d
aniebietafia <aniebietafia87@gmail.com>
2024-09-10 09:47:44 +0100

    feat: generate new verification token

--- brints-estate-api/src/users/providers/generate-new-email-verification.provider.ts
+++ brints-estate-api/src/users/providers/generate-new-email-verification.provider.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
+import { Repository } from 'typeorm';
+import { UserAuth } from '../entities/userAuth.entity';
+import { GenerateTokenHelper } from 'src/utils/generate-token.lib';
+import { GenerateNewEmailTokenDto } from '../dto/new-email-token.dto';
+import { CustomException } from 'src/exceptions/custom.exception';
+import { VerificationStatus } from 'src/enums/roles.model';
 
 @Injectable()
-export class GenerateNewEmailVerificationProvider {}
+export class GenerateNewEmailVerificationProvider {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+    @InjectRepository(UserAuth)
+    private readonly userAuthRepository: Repository<UserAuth>,
+
+    private readonly generateTokenHelper: GenerateTokenHelper,
+  ) {}
+
+  public async newEmailVerificationToken(
+    generateNewEmailTokenDto: GenerateNewEmailTokenDto,
+  ): Promise<void> {
+    const user = await this.userRepository.findOne({
+      where: { email: generateNewEmailTokenDto.email },
+      relations: { user_auth: true },
+    });
+
+    if (!user)
+      throw new CustomException(HttpStatus.NOT_FOUND, 'User does not exist');
+
+    const userAuth = await this.userAuthRepository.findOne({
+      where: { id: user.user_auth.id },
+    });
+
+    if (!userAuth)
+      throw new CustomException(HttpStatus.NOT_FOUND, 'Please, try again');
+
+    if (user.isVerified || userAuth.isEmailVerified)
+      throw new CustomException(
+        HttpStatus.BAD_REQUEST,
+        'Email is already verified',
+      );
+
+    if (
+      userAuth.emailVerificationTokenExpiresIn &&
+      userAuth.emailVerificationTokenExpiresIn > new Date()
+    )
+      throw new CustomException(
+        HttpStatus.FORBIDDEN,
+        'You have an active verification token',
+      );
+
+    const verificationToken =
+      this.generateTokenHelper.generateVerificationToken();
+    const verificationTokenExpiry = new Date();
+    verificationTokenExpiry.setHours(verificationTokenExpiry.getHours() + 1);
+
+    userAuth.emailVerificationToken = verificationToken;
+    userAuth.emailVerificationTokenExpiresIn = verificationTokenExpiry;
+    userAuth.email_status = VerificationStatus.PENDING;
+    await this.userAuthRepository.save(userAuth);
+  }
+}
