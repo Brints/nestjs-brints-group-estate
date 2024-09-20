f822752aba218557b4a511274a3e4bf25e45cc07
aniebietafia <aniebietafia87@gmail.com>
2024-09-20 16:31:57 +0100

    feat: login attempts module

--- brints-estate-api/src/login-attempts/login-attempts.module.ts
+++ brints-estate-api/src/login-attempts/login-attempts.module.ts
@@ -2,7 +2,7 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoginAttempts } from './entities/login-attempt.entity';
 import { LoginAttemptsProvider } from './providers/login-attempts.provider';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   providers: [LoginAttemptsProvider],
