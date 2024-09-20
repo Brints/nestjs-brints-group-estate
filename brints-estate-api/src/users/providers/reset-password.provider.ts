e9c2b8568f9b5ea030629bb847bd0b27e2e2dc7d
aniebietafia <aniebietafia87@gmail.com>
2024-09-20 16:29:53 +0100

    feat: reset password provider

---
+++ brints-estate-api/src/users/providers/reset-password.provider.ts
@@ -0,0 +1,104 @@
+import { forwardRef, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { UserAuth } from '../entities/userAuth.entity';
+import { HashingProvider } from '../../auth/providers/hashing.provider';
+import { ResetPasswordDto } from '../dto/reset-password.dto';
+import { CustomException } from '../../exceptions/custom.exception';
+import { AccountStatus } from '../../enums/roles.model';
+
+@Injectable()
+export class ResetPasswordProvider {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+    @InjectRepository(UserAuth)
+    private readonly userAuthRepository: Repository<UserAuth>,
+
+    @Inject(forwardRef(() => HashingProvider))
+    private readonly hashingProvider: HashingProvider,
+  ) {}
+
+  public async resetPassword(
+    params: string[],
+    resetPasswordDto: ResetPasswordDto,
+  ): Promise<void> {
+    const user = await this.userRepository.findOne({
+      where: { email: params[0] },
+      relations: { user_auth: true },
+    });
+
+    if (!user)
+      throw new CustomException(HttpStatus.NOT_FOUND, 'User does not exist.');
+
+    if (!user.isVerified)
+      throw new CustomException(
+        HttpStatus.FORBIDDEN,
+        'Verify your account first.',
+      );
+
+    if (
+      user.account_status === AccountStatus.BLOCKED ||
+      user.account_status === AccountStatus.SUSPENDED
+    )
+      throw new CustomException(
+        HttpStatus.FORBIDDEN,
+        'Your account is suspended or block. Please, contact the admin.',
+      );
+
+    if (resetPasswordDto.new_password !== resetPasswordDto.confirm_password)
+      throw new CustomException(
+        HttpStatus.BAD_REQUEST,
+        'Passwords do not match.',
+      );
+
+    const userAuth = await this.userAuthRepository.findOne({
+      where: { id: user.user_auth.id },
+    });
+
+    if (!userAuth)
+      throw new CustomException(
+        HttpStatus.NOT_FOUND,
+        'User Auth does not exist',
+      );
+
+    if (userAuth.passwordResetToken !== params[1])
+      throw new CustomException(
+        HttpStatus.BAD_REQUEST,
+        'Invalid reset password token.',
+      );
+
+    if (
+      userAuth.passwordResetTokenExpiresIn &&
+      userAuth.passwordResetTokenExpiresIn < new Date()
+    )
+      throw new CustomException(
+        HttpStatus.BAD_REQUEST,
+        'Password reset token has expired.',
+      );
+
+    const isOldPassword = await this.hashingProvider.comparePassword(
+      resetPasswordDto.new_password,
+      user.password,
+    );
+
+    if (isOldPassword)
+      throw new CustomException(
+        HttpStatus.FORBIDDEN,
+        'Your new password cannot be same as the old password',
+      );
+
+    const hashedPassword = await this.hashingProvider.hashPassword(
+      resetPasswordDto.new_password,
+    );
+
+    user.password = hashedPassword;
+    userAuth.passwordResetToken = null;
+    userAuth.passwordResetTokenExpiresIn = null;
+
+    await this.userAuthRepository.save(userAuth);
+    await this.userRepository.save(user);
+  }
+}
