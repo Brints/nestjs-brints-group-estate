bff99beb6a803a65abea756ee3067c524806df5e
aniebietafia <aniebietafia87@gmail.com>
2024-09-24 11:21:22 +0100

    feat: create user dto

---
+++ brints-estate-api/src/auth/dto/create-user.dto.ts
@@ -0,0 +1,98 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import {
+  IsBoolean,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { UserGender } from '../../enums/gender.enum';
+
+export class CreateUserDto {
+  @ApiProperty({
+    description: 'First name of the user',
+    example: 'John',
+    type: String,
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(255)
+  first_name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(255)
+  last_name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEmail()
+  @MaxLength(255)
+  @Transform(({ value }) => value.toLowerCase())
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @Matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])/, {
+    message: 'password too weak',
+  })
+  password: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @Matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])/, {
+    message: 'password too weak',
+  })
+  confirm_password: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @Matches(/^[0-9]{1,5}$/, {
+    message: 'Please enter a correct country code',
+  })
+  country_code: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(50)
+  phone_number: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEnum(UserGender)
+  @Transform(({ value }) => value.toLowerCase())
+  gender: UserGender;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsBoolean()
+  @Transform(({ value }) => value === 'true')
+  terms_and_conditions: boolean;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsBoolean()
+  @Transform(({ value }) => value === 'true')
+  privacy_policy: boolean;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsBoolean()
+  @Transform(({ value }) => value === 'true')
+  marketing: boolean;
+}
