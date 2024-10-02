c5d4c25f0b6e30e8c0ae41187624a77d89e30568
aniebietafia <aniebietafia87@gmail.com>
2024-10-02 20:15:29 +0100

    feat: update user dto

--- brints-estate-api/src/users/dto/update-user.dto.ts
+++ brints-estate-api/src/users/dto/update-user.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from '../../auth/dto/create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({
+    description: 'The id of the user',
+    example: '60b7f3a8d8e9a7e4d8f9b1a7',
+  })
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+}
