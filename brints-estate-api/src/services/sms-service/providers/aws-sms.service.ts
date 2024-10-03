79708ad63be0d60c98f311fd270da4443aa0a6e5
aniebietafia <aniebietafia87@gmail.com>
2024-10-03 17:45:48 +0100

    sms: send otp sms

--- brints-estate-api/src/services/sms-service/providers/aws-sms.service.ts
+++ brints-estate-api/src/services/sms-service/providers/aws-sms.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { AwsSmsProvider } from './aws-sms.provider';
 import { TimeHelper } from 'src/utils/time-helper.lib';
+import { User } from 'src/users/entities/user.entity';
+import { UserAuth } from 'src/users/entities/userAuth.entity';
 
 @Injectable()
 export class AwsSmsService {
@@ -10,13 +12,9 @@ export class AwsSmsService {
     private readonly timeHelper: TimeHelper,
   ) {}
 
-  public async sendOTPSms(
-    fullname: string,
-    phoneNumber: string,
-    otpExpiry: Date | null,
-    otp: number | null,
-  ): Promise<void> {
-    const message = `Hey ${fullname}. Thank you for signing up on Brints. Kindly use this OTP ${otp} to verify your phone number. This OTP expires in ${this.timeHelper.getTimeLeft(otpExpiry, 'minutes')}.`;
-    await this.awsSmsProvider.sendSms(phoneNumber, message);
+  public async sendOTPSms(user: User, userAuth: UserAuth): Promise<void> {
+    const fullname = `${user.first_name} ${user.last_name}`;
+    const message = `Hey ${fullname}. Thank you for signing up on Brints. Kindly use this OTP ${userAuth.otp} to verify your phone number. This OTP expires in ${this.timeHelper.getTimeLeft(userAuth.otpExpiresIn, 'minutes')}.`;
+    await this.awsSmsProvider.sendSms(user.phone_number, message);
   }
 }
