964acaa9eabb172a1bb98249d01cd721c439d6fb
aniebietafia <aniebietafia87@gmail.com>
2024-10-06 19:42:30 +0100

    test: initializing update user unit test

---
+++ brints-estate-api/src/users/providers/update-user.provider.spec.ts
@@ -0,0 +1,4 @@
+import { DataSource, Repository } from 'typeorm';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UpdateUserProvider } from './update-user.provider';
