421579fe24c7272fae8efe3156a85bf868e91991
aniebietafia <aniebietafia87@gmail.com>
2024-11-04 17:31:56 +0100

    doc: Documentation for verify phone number endpoint.

--- brints-estate-api/src/users/swagger_docs/verify-phone-response.doc.ts
+++ brints-estate-api/src/users/swagger_docs/verify-phone-response.doc.ts
@@ -17,3 +17,113 @@ export const VerifyPhoneResponse = {
     },
   },
 };
+
+export const NotFoundPhoneNumberResponse = {
+  description: 'Phone number does not exist',
+  status: HttpStatus.NOT_FOUND,
+  schema: {
+    type: 'object',
+    properties: {
+      success: {
+        type: 'boolean',
+        example: false,
+      },
+      status_code: {
+        type: 'number',
+        example: HttpStatus.NOT_FOUND,
+      },
+      message: {
+        type: 'string',
+        example: 'Phone number does not exist.',
+      },
+    },
+  },
+};
+
+export const IncorrectPhoneNumberResponse = {
+  description: 'Incorrect phone number',
+  status: HttpStatus.BAD_REQUEST,
+  schema: {
+    type: 'object',
+    properties: {
+      success: {
+        type: 'boolean',
+        example: false,
+      },
+      status_code: {
+        type: 'number',
+        example: HttpStatus.BAD_REQUEST,
+      },
+      message: {
+        type: 'string',
+        example: 'Incorrect phone number.',
+      },
+    },
+  },
+};
+
+export const ForbiddenAccountVerifiedResponse = {
+  description: 'This account is already verified',
+  status: HttpStatus.FORBIDDEN,
+  schema: {
+    type: 'object',
+    properties: {
+      success: {
+        type: 'boolean',
+        example: false,
+      },
+      status_code: {
+        type: 'number',
+        example: HttpStatus.FORBIDDEN,
+      },
+      message: {
+        type: 'string',
+        example: 'This account is already verified.',
+      },
+    },
+  },
+};
+
+export const InvalidOTPResponse = {
+  description: 'Invalid OTP. Try again.',
+  status: HttpStatus.BAD_REQUEST,
+  schema: {
+    type: 'object',
+    properties: {
+      success: {
+        type: 'boolean',
+        example: false,
+      },
+      status_code: {
+        type: 'number',
+        example: HttpStatus.BAD_REQUEST,
+      },
+      message: {
+        type: 'string',
+        example: 'Invalid OTP. Try again.',
+      },
+    },
+  },
+};
+
+export const OTPExpiredResponse = {
+  description: 'OTP has expired. Please, generate a new one.',
+  status: HttpStatus.BAD_REQUEST,
+  schema: {
+    type: 'object',
+    properties: {
+      success: {
+        type: 'boolean',
+        example: false,
+      },
+      status_code: {
+        type: 'number',
+        example: HttpStatus.BAD_REQUEST,
+      },
+      message: {
+        type: 'string',
+        example: 'OTP has expired. Please, generate a new one.',
+      },
+    },
+  },
+};
